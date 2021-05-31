8872b7c26ef83ce72ed7365740d9138b6bc79e3e
AnoshkinDV <AnoshkinDV@mpei.ru>
2021-05-31 16:17:22 +0300

    установил библиотеку

--- cs-lab03/main.cpp
+++ cs-lab03/main.cpp
@@ -2,12 +2,13 @@
 #include <vector>
 #include "histogram.h"
 #include "svg.h"
+#include <curl/curl.h>
 
 using namespace std;
 
 
 int main() {
-
+    curl_global_init(CURL_GLOBAL_ALL);
     const auto input = read_input(cin, true);
     const auto bins = make_histogram(input);
     show_histogram_svg(bins);
