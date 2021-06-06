c5d6d4f4c7b94b610dc58e7bff5ee022a440da81
AnoshkinDV <AnoshkinDV@mpei.ru>
2021-06-06 20:17:40 +0300

    загрузка файла по сети

--- cs-lab03/main.cpp
+++ cs-lab03/main.cpp
@@ -7,9 +7,20 @@
 using namespace std;
 
 
-int main() {
-    curl_global_init(CURL_GLOBAL_ALL);
+int main(int argc, char** argv) {
+    if (argc > 1)
+    {  curl_global_init(CURL_GLOBAL_ALL);
+       CURL *curl = curl_easy_init();
+if(curl) {
+  CURLcode res;
+  curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+  res = curl_easy_perform(curl);
+  curl_easy_cleanup(curl);
+}
+       return 0;
+    }
     const auto input = read_input(cin, true);
     const auto bins = make_histogram(input);
     show_histogram_svg(bins);
+    return 0;
 }
